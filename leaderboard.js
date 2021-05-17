0cd6582e856e3cdedf91da4abee5e65d4990a479
Marc <marc.ferraro1@gmail.com>
2021-05-17 15:19:02 -0500

    leaderboard.js cleanup done

--- leaderboard.js
+++ leaderboard.js
@@ -1,24 +1,4 @@
-// const fetchStatistics = () => {
-//     fetchData('http://localhost:3000/leaderboard')
-//     .then(users => renderLeaderboards(users))
-// }
-
-// const renderLeaderboards = (users) => {
-//     // users.forEach(attempt => console.log)
-//     console.table(users)
-
-//     mostPuzzlesCompleted(users)
-// }
-
-// const mostPuzzlesCompleted = (users) => {
-//     // Math
-//     debugger
-// }
-
-
-
 const displayLeaderboard = () => {
-
     const leaderboardDiv = document.createElement('div')
     const leaderboardHeader = document.createElement('h1')
     leaderboardHeader.innerText = `Geo Hunt Player Stats`
@@ -52,60 +32,51 @@ const displayLeaderboard = () => {
 
     fetchData(userUrl)
     .then(users => {
-      
-        users.forEach(user =>
-            {
-                // console.log(user)
-                // console.log(user.attempts.length)
-                const tr = document.createElement('tr')
-                tr.dataset.id = user.id
-                tr.addEventListener("click", displayUserProfile)
+      users.forEach(user =>
+        {
+          const tr = document.createElement('tr')
+          tr.dataset.id = user.id
+          tr.addEventListener("click", displayUserProfile)
 
+          const body = document.querySelector('tbody')
 
-                const body = document.querySelector('tbody')
+          const failedAttempts = user.attempts.filter(attempt => attempt.status === "Failed")
 
-                // let userAttempts = user.attempts
-                const failedAttempts = user.attempts.filter(attempt => attempt.status === "Failed")
-                // console.log(failedAttempts.length)
+          const completedAttempts = user.attempts.filter(attempt => attempt.status === "Completed")
 
-                const completedAttempts = user.attempts.filter(attempt => attempt.status === "Completed")
-                // console.log(completedAttempts.length)
-
-
-                let playTime = completedAttempts.map(attempt => attempt.time_taken)
-                let maxPlayTime
-                let minPlayTime
-                if (playTime.length < 1){
-                  maxPlayTime = "n/a"
-                  minPlayTime = "n/a"
-                  tr.innerHTML = 
-                `
-                <td>${user.username}</td>
-                <td>${user.name}</td>
-                <td>${user.attempts.length}</td>
-                <td>${completedAttempts.length}</td>
-                <td>${failedAttempts.length}</td>
-                <td>${minPlayTime}</td>
-                <td>${maxPlayTime}</td>
-                `
-                } else {
-                  maxPlayTime = Math.max(...playTime)
-                  minPlayTime = Math.min(...playTime)
-                  tr.innerHTML = 
-                `
-                <td>${user.username}</td>
-                <td>${user.name}</td>
-                <td>${user.attempts.length}</td>
-                <td>${completedAttempts.length}</td>
-                <td>${failedAttempts.length}</td>
-                <td>${minPlayTime} seconds</td>
-                <td>${maxPlayTime} seconds</td>
-                `
-                }
-                
-                body.append(tr)
-            })
-        
+          let playTime = completedAttempts.map(attempt => attempt.time_taken)
+          let maxPlayTime
+          let minPlayTime
+          if (playTime.length < 1){
+            maxPlayTime = "n/a"
+            minPlayTime = "n/a"
+            tr.innerHTML = 
+          `
+          <td>${user.username}</td>
+          <td>${user.name}</td>
+          <td>${user.attempts.length}</td>
+          <td>${completedAttempts.length}</td>
+          <td>${failedAttempts.length}</td>
+          <td>${minPlayTime}</td>
+          <td>${maxPlayTime}</td>
+          `
+          } else {
+            maxPlayTime = Math.max(...playTime)
+            minPlayTime = Math.min(...playTime)
+            tr.innerHTML = 
+          `
+          <td>${user.username}</td>
+          <td>${user.name}</td>
+          <td>${user.attempts.length}</td>
+          <td>${completedAttempts.length}</td>
+          <td>${failedAttempts.length}</td>
+          <td>${minPlayTime} seconds</td>
+          <td>${maxPlayTime} seconds</td>
+          `
+          }
+          
+          body.append(tr)
+        })
     })
 }
 
@@ -114,8 +85,6 @@ const displayUserProfile = (e) => {
     interfaceDiv.innerHTML = ""
     fetchData(userUrl + `/${e.target.parentElement.dataset.id}`)
     .then(user => {
-      
-        // console.log(user)
         const username = document.createElement('h1')
         username.innerText = `${user.username}`
         username.className = 'text-center'
@@ -162,6 +131,5 @@ const displayUserProfile = (e) => {
           body.append(tr)
           
         })
-
     })
 }
